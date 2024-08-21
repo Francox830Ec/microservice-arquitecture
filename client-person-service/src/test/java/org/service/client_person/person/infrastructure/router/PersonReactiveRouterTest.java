36474138ccb9fe249baf323dfa832f88fc6ff988
Francisco Xavier Yambay <francox830@gmail.com>
2024-08-21 16:21:04 -0500

    Add requested unit test to the sources

--- client-person-service/src/test/java/org/service/client_person/person/infrastructure/router/PersonReactiveRouterTest.java
+++ client-person-service/src/test/java/org/service/client_person/person/infrastructure/router/PersonReactiveRouterTest.java
@@ -1,4 +1,56 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.service.client_person.person.infrastructure.router;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.service.client_person.person.domain.model.PersonDTO;
+import org.service.client_person.person.domain.port.out.IPersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class PersonReactiveRouterTest {
-  
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean
+    IPersonRepository repository;
+
+    @Test
+    void testPersonReactiveRouter() {
+        PersonReactiveRouter router = new PersonReactiveRouter();
+        assertNotNull(router);
+    }
+
+    @Test
+    void testCreatePersonOk() {
+        PersonDTO requestPersonDTO = buildRequestPersonDTO();
+        PersonDTO savedPersonDTO = buildSavedPersonDTO(requestPersonDTO);
+
+        Mockito.when(repository.create(requestPersonDTO)).thenReturn(savedPersonDTO);
+
+        webTestClient.post()
+                .uri("/person")
+                .body(Mono.just(requestPersonDTO), PersonDTO.class)
+                .exchange()
+                .expectBody(PersonDTO.class);
+    }
+
+    private static PersonDTO buildRequestPersonDTO() {
+        return new PersonDTO(null, "person1", "M", 18, "123456789", "NA", "1800100100");
+    }
+
+    private static PersonDTO buildSavedPersonDTO(PersonDTO requestPersonDTO) {
+        return new PersonDTO(UUID.randomUUID(), requestPersonDTO.name(), requestPersonDTO.gender(),
+                        requestPersonDTO.age(), requestPersonDTO.identification(), requestPersonDTO.address(),
+                        requestPersonDTO.phone());
+    }
 }
